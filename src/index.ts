9d168148d4451e93c4a0e088dc555dd1a02448dc
AndreMotta25 <andremotta16041998@gmail.com>
2023-09-15 00:52:39 -0300

    refactor:removing the onload event

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import { Translator } from "./entities/Translator";
 
-window.onload = () => {
-    document.head.innerHTML += '<script defer src="https://kit.fontawesome.com/2b6ab9892c.js" crossorigin="anonymous"></script>'
-}
-
 const translate = new Translator()
 translate.init();
+
